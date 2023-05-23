445002a68d7005905f380ac0aae631deb3aa2ff2
l-k-sfr-jt <lukas3197@gmail.com>
2023-05-23 19:10:07 +0200

    Initial Commit

---
+++ cats/catsApp.swift
@@ -0,0 +1,17 @@
+//
+//  catsApp.swift
+//  cats
+//
+//  Created by Lukáš Frajt on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct catsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
